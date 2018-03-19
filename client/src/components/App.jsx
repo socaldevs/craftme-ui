5bd988fce7ae684fcd2b143d32c287db3d906fc0
Eddie Huang <ehuange@gmail.com>
2018-03-19 12:15:32 -0700

    [Router] - Implements React Router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
+import UserList from "../containers/user-list.jsx";
+import UserDetail from "../containers/user-detail.jsx";
+import { Switch, Route, Link } from "react-router-dom";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
 
-import UserList from '../containers/user-list.jsx';
-import UserDetail from '../containers/user-detail.jsx';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-const App = () => (
-  <div>
-      <h2>User List</h2>
-      <UserList />
-      <hr />
-      <h2>User Details</h2>
-      <UserDetail />
-  </div>
-);
-
-export default App;
-
-
+  render() {
+    return (
+      <header>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+        <div>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </header>
+    );
+  }
+}
---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        This is the Signup
+        <form />
+      </div>
+    );
+  }
+}
