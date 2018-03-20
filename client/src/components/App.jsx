bb39b07ea50f93248954e0ab16bbb3b73e82647f
Brian Duong <brndng89@gmail.com>
2018-03-20 11:10:06 -0700

    [chat] Basic chat implementation w/ test code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
 import UserList from '../containers/user-list.jsx';
 import UserDetail from '../containers/user-detail.jsx';
-<<<<<<< HEAD
 import { Switch, Route, Link } from 'react-router-dom';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
-=======
-import Chat from './Chat.jsx';
 import ChatRoomList from './ChatRoomList.jsx';
-
-const App = () => (
-  <div>
-      <h2>User List</h2>
-      <UserList />
-      <hr />
-      <h2>User Details</h2>
-      <UserDetail />
-      <hr />
-      <ChatRoomList />
-  </div>
-);
->>>>>>> [rooms] made room list, click to enter, dynamic room id passed through props, lifecycle timings
 
 export default class App extends Component {
   constructor(props) {
@@ -38,16 +22,22 @@ export default class App extends Component {
           <li>
             <Link to="/signup">Signup</Link>
           </li>
+          <li>
+            <Link to="/chatrooms">ChatRooms</Link>
+          </li>
         </ul>
         <div>
           <Switch>
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route path="/chatrooms" component={ChatRoomList} />
           </Switch>
+        </div>
+        <div>
+          
         </div>
       </header>
     );
   }
 }
 
-export default App;
