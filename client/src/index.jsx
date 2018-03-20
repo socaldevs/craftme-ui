89df19753cff17c6afc16e34426bdd5c1f1daeed
John <yahya.al.mashni@mail.utoronto.ca>
2018-03-19 17:01:07 -0700

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,12 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import allReducers from "./reducers/index.jsx";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import App from "./components/App.jsx";
+import App from './components/App.jsx';
 
-const store = createStore(allReducers);
-console.log("this is what store looks like!", store.getState());
+const store = createStore(allReducers, composeWithDevTools());
+console.log('this is what store looks like!', store.getState());
 
 ReactDOM.render(
   <BrowserRouter>
