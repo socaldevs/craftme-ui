36852286784bf61472af3f45ed9a84f7a50a500f
Eddie Huang <ehuange@gmail.com>
2018-03-22 11:56:45 -0700

    [Rebase] - Commit for rebase

--- client/src/components/Lessons.jsx
+++ client/src/components/Lessons.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 export default class Lessons extends Component {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount() {}
+
   render() {
     return <div>This is the lessons component</div>;
   }
