7e784cd54b089a06ada40dd60bf270955287d150
Brian Duong <brndng89@gmail.com>
2018-03-22 10:37:41 -0700

    [code] code cleanup on ui

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -10,6 +10,8 @@ class Chat extends Component {
       message: '',
       messages: [],
       feedback: '',
+      peerId: '',
+      otherPeerId: '',
     }
   }
   componentDidMount() { 
@@ -17,7 +19,18 @@ class Chat extends Component {
     this.socket.on('connect', () => {
       this.socket.emit('room', this.props.roomId);
     });
-    this.socket.on('message', (data) => console.log('message from server', data));
+    this.socket.on('confirmation', (data) => {
+      console.log('message from server:', data)
+      if (this.state.messages.length !== 0) {
+        this.socket.emit('renderchat', {
+          messages: this.state.messages,
+          room: this.props.roomId,
+        });
+      } 
+    });
+    this.socket.on('renderchat', (data) => {
+      this.setState({ messages: [...data]});
+    })
     this.socket.on('chat', (data) => {
       this.setState({ 
         messages: [...this.state.messages, data], 
@@ -27,6 +40,35 @@ class Chat extends Component {
     this.socket.on('typing', (data) => {
       this.setState({ feedback: data});
     })
+    this.socket.on('getOtherPeerId', (data) => {
+      this.socket.emit('fetchedPeerId', {
+        peerId: this.state.peerId,
+        room: this.props.roomId,
+      });
+    });
+    this.socket.on('fetchedPeerId', (data) => {
+      this.setState({ otherPeerId: data });
+    });
+
+    //P2P
+    this.peer = new Peer({key: 'lwjd5qra8257b9'});
+    this.peer.on('open', (id) => {
+      this.setState({ peerId: id });
+    });
+    this.peer.on('call', (call) => {
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      navigator.getUserMedia({video: true, audio: true}, (stream) => {
+        call.answer(stream); 
+        call.on('stream', (remoteStream) => {
+          let video = document.createElement('video');
+          document.body.append(video);
+          video.src = window.URL.createObjectURL(remoteStream);
+          video.play();
+        });
+      }, (err) => {
+        console.log('Failed to get local stream', err);
+      });
+    });
   }
 
   componentWillReceiveProps(nextProps) {
@@ -41,7 +83,6 @@ class Chat extends Component {
     const { messages } = this.state;
     try {
       const data = await axios.post('http://localhost:3001/chat/save/', { messages });
-      console.log('await axios data', data);
     } catch(err) {
       console.log('err from Chat', err);
     }
@@ -51,7 +92,6 @@ class Chat extends Component {
     const testId = '5ab06c447ee8e1cddeddd726'; //TEST
     try {
       const data = await axios.get(`http://localhost:3001/chat/fetch/${testId}`);
-      console.log('await axios data', data);
     } catch(err) {
       console.log('err from Chat', err);
     }
@@ -77,6 +117,27 @@ class Chat extends Component {
     });
   }
 
+  getOtherPeerId() {
+    this.socket.emit('getOtherPeerId', this.props.roomId);
+  }
+
+  callPeer() { 
+    const peer = this.peer;
+    const { otherPeerId } = this.state;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    navigator.getUserMedia({video: true, audio: true}, (stream) => {
+      const call = peer.call(otherPeerId, stream);
+      call.on('stream', (remoteStream) => {
+        let video = document.createElement('video');
+        document.body.append(video);
+        video.src = window.URL.createObjectURL(remoteStream);
+        video.play();
+      });
+    }, (err) => {
+      console.log('Failed to get local stream', err);
+    });
+  }
+
   render() {
     return (
       <div>
@@ -94,6 +155,8 @@ class Chat extends Component {
         <button id="send" onClick={() => this.sendChat()}>Send</button>
         <button id="save" onClick={() => this.saveChat()}>SAVE CHAT</button>
         <button id="fetch" onClick={() => this.fetchChat()}>RETRIEVE CHAT</button>
+        <button id="get" onClick={() => this.getOtherPeerId()}>Get PeerId</button>
+        <button id="call" onClick={() => this.callPeer()}>Call Peer</button>
       </div>
     );
   } 
