bc16ffedf728e5c67c14fc1bf46b818011494d72
fireArthur <thefirearthur@gmail.com>
2018-03-27 08:54:38 -0700

    [Card] creates a card component

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,22 @@
+export class Card extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+      
+    // };
+  }
+
+  // clickHandler(){
+  //   this.props.history.push('/calendar');
+  // }
+
+  render() {
+    <div>
+      <img src="" alt=""/>
+      <h3>Title</h3>
+      <p>Just a little paragraph</p>
+         
+      <button type="button">Button</button>
+    </div>
+  }
+};
