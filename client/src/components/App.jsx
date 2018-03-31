57157ff9e2b339fb0be94cab6a2704a4e0d947d2
John <yahya.al.mashni@mail.utoronto.ca>
2018-03-31 11:23:17 -0700

    [Front] fixed two files out of 4 for ESLINT

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,27 +1,17 @@
-import { Component } from 'react';
-import React from 'react';
-import UserList from '../containers/user-list.jsx';
-import UserDetail from '../containers/user-detail.jsx';
-import { Switch, Route, Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Protected from './Protected.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
-import Lessons from './Lessons.jsx';
 import Messages from './Messages.jsx';
 import Feedback from './Feedback.jsx';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Paper from 'material-ui/Paper';
 import Grid from './Grid.jsx';
-import Button from 'material-ui/Button';
-import Menu, { MenuItem } from 'material-ui/Menu';
-import axios from 'axios';
 import Search from './Search.jsx';
-import ReactModal from 'react-modal';
 import LessonsContainer from './LessonsContainer.jsx';
 import Conference from './Conference.jsx';
 import SearchResults from './SearchResults.jsx';
 import Calendar from './Calendar.jsx';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -31,28 +21,25 @@ class App extends Component {
 
   render() {
     return (
-    <div >
-  <Switch>
-  <Route path="/messages" component={props => (<Protected component={Messages} {...props} />)}/>
-  <Route path="/lessons" component={props => (<Protected component={LessonsContainer} {...props} />)}/>
-  <Route path="/feedback" component={props => (<Protected component={Feedback} {...props} />)}/>
-  <Route path="/messages" component={props => (<Protected component={Messages} {...props} />)}/>
-  <Route path="/search" component={props => (<Protected component={Search} {...props} />)}/>
-  <Route path="/conference" component={props => (<Protected component={Conference} {...props} />)}/>
-  {/* <Route path="/lessonsContainer" component={props => (<Protected component={LessonsContainer} {...props} />)}/> */}
-  {/* <Route path="/lessonsContainer" component={LessonsContainer} /> */}
-  <Route path="/searchResults" component={props => (<Protected component={SearchResults} {...props} />)}/>
-  <Route path="/calendar" component={props => (<Protected component={Calendar} {...props} />)}/>
-  <Route path="/grid" component={Grid} />
-  <Route path="/signup" component={Signup} />
-  <Route path="/login" component={Login} />
-  <Route path="/" component={props => (<Protected component={Search} {...props} />)}/>
-  </Switch>
-    </div>
-    
+      <div >
+        <Switch>
+          <Route path="/messages" component={props => (<Protected component={Messages} {...props} />)} />
+          <Route path="/lessons" component={props => (<Protected component={LessonsContainer} {...props} />)} />
+          <Route path="/feedback" component={props => (<Protected component={Feedback} {...props} />)} />
+          <Route path="/messages" component={props => (<Protected component={Messages} {...props} />)} />
+          <Route path="/search" component={props => (<Protected component={Search} {...props} />)} />
+          <Route path="/conference" component={props => (<Protected component={Conference} {...props} />)} />
+          <Route path="/searchResults" component={props => (<Protected component={SearchResults} {...props} />)} />
+          <Route path="/calendar" component={props => (<Protected component={Calendar} {...props} />)} />
+          <Route path="/grid" component={Grid} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/" component={props => (<Protected component={Search} {...props} />)} />
+        </Switch>
+      </div>
+
 
     );
   }
 }
-
-export default App
+export default App;
